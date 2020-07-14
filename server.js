310b900ccc590032b3cd21d89c1d9434fc4a6601
Yosef Silberhaft <yosef.silberhaft@ironsrc.com>
2020-07-14 19:23:43 +0300

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const {isAuthorized} = require('./src/middlewares/isAuthorized');
 const {findAppById, searchApps} = require('./src/app/scraper');
 const {getBearerToken} = require('./src/app/auth');
+const cors = require('cors');
 
 const express = require('express');
 const server = express();
+server.use(cors());
 
 server.get('/app/:appId', isAuthorized, async (req, res) => {
     return findAppById(req, res);
