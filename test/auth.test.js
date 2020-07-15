d530603d3475c5338a81bc852bc1e0e1c4acd3ac
Yosef Silberhaft <yosef.silberhaft@ironsrc.com>
2020-07-15 15:40:21 +0300

    Updated the test structure

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,46 +1,57 @@
 const {getBearerToken} = require('../src/app/auth')
 const faker = require('faker')
 const sinon = require("sinon")
+const axios = require('axios')
 
 let req
 let resSpy
+let serverUrl = 'http://localhost:3000'
 
 describe('Verifying getBearerToken', () => {
 
 	beforeAll(() => {
-		console.log('Tests starting')
 		req = {method: 'GET'}
 		resSpy = {}
 	})
 
-	describe('The getBearerToken Function', () => {
-		describe('Validate Input', () => {
-			beforeEach(() => {
-				req.query = {}
-				resSpy.send = sinon.spy()
-				resSpy.status = sinon.spy()
-			})
+	describe('Validate JWT Token Generation', () => {
+		beforeEach(() => {
+			req.query = {}
+			resSpy.send = sinon.spy()
+			resSpy.status = sinon.spy()
+		})
 
-			it('Correct Params', async () => {
+		describe('Validate the use of the getBearerToken() function ', () => {
+			it('Should pass function test', async () => {
 				req['query']['userName'] = faker.name.findName()
 				req['query']['password'] = faker.internet.password()
-				const token = await getBearerToken(req, resSpy)
+				await getBearerToken(req, resSpy)
 				expect(resSpy.send.args[0][0]).toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
 			})
 
-			it('Missing Username', async () => {
+			it('Should fail missing username', async () => {
 				req['query']['password'] = faker.internet.password()
 				await getBearerToken(req, resSpy)
 				expect(resSpy.status.args[0][0]).toBe(400)
 				expect(resSpy.send.args[0][0]).toBe("userName and password are missing")
 			})
 
-			it('Username misspelled', async () => {
+			it('Should fail username misspelled', async () => {
 				req['query']['username'] = faker.name.findName()
 				req['query']['password'] = faker.internet.password()
 				await getBearerToken(req, resSpy)
 				expect(resSpy.status.args[0][0]).toBe(400)
 				expect(resSpy.send.args[0][0]).toBe("userName and password are missing")
+			})
+		})
+
+		describe('Validate the /auth endpoint', () => {
+			it('Should pass with code 200 and issued token', async () => {
+				const userName = faker.name.findName()
+				const password = faker.internet.password()
+				const response = await axios.get(`${serverUrl}/auth`, { params: {userName: userName, password: password}})
+				expect(response.status).toBe(200)
+				expect(response.data).toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
 			})
 		})
 	})
--- test/isAuthorized.test.js
+++ test/isAuthorized.test.js
@@ -8,15 +8,12 @@ let resSpy = {}
 let authToken
 let nextSpy
 
-describe('Testing the isAuthorized middleware', () => {
-	beforeAll(() => {
-		console.log('Tests starting')
-	})
+describe('Verifying the isAuthorized middleware', () => {
 
 	describe('Test isAuthorized Function', () => {
 
-		describe('Validate Input', () => {
-			beforeAll(async () => {
+		describe('Checking valid inputs', () => {
+			beforeEach(async () => {
 				req = {method: 'GET', query: {}, headers: {}}
 				resSpy.send = sinon.spy()
 				resSpy.status = sinon.spy()
@@ -27,36 +24,27 @@ describe('Testing the isAuthorized middleware', () => {
 				authToken = resSpy.send.args[0][0]
 				req['headers']['authorization'] = authToken
 			})
-			it('Authorized User', async () => {
+			it('Should pass user is authorized', async () => {
 				await isAuthorized(req, resSpy, nextSpy)
 				expect(nextSpy.firstCall.callId).toBe(1)
 			})
 		})
 
-		describe('Not authorization in Header', () => {
+		describe('Checking with invalid inputs', () => {
 			beforeEach(() => {
 				req = {method: 'GET', query: {}, headers: {}}
 				resSpy.send = sinon.spy()
 				resSpy.status = sinon.spy()
 				nextSpy = sinon.spy()
 			})
-			it('Missing JWT Token', async () => {
+			it('Should Fail no authorization in Header', async () => {
 				await isAuthorized(req, resSpy, nextSpy)
 				expect(resSpy.status['args'][0][0]).toBe(401)
 				expect(resSpy.send['args'][0][0]).toBe('Unauthorized user')
 				expect(resSpy.send['args'][1][0].message).toBe('jwt must be provided')
 			})
-		})
-
-		describe('Invalid JWT Signature', () => {
-			beforeEach(() => {
-				req = {method: 'GET', query: {}, headers: {}}
-				resSpy.send = sinon.spy()
-				resSpy.status = sinon.spy()
-				nextSpy = sinon.spy()
+			it('Should fail invalid JWT signature', async () => {
 				req.headers['authorization'] = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyaWQiOiJqb2huLmRvZSJ9.CuScq77_iCP4XsYGCMgGnQiATOmQwu_rR1LEB2Pcd_I'
-			})
-			it('Invalid Signature', async () => {
 				await isAuthorized(req, resSpy)
 				expect(resSpy.send.args[0][0].name).toBe('JsonWebTokenError')
 				expect(resSpy.send.args[0][0].message).toBe('invalid signature')
